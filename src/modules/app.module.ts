1f01c4adea3c641fc2533fbfc472555021cd9506
Максим <jakdman2003@gmail.com>
2024-02-25 18:17:18 +0300

    initial commit

---
+++ src/modules/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'cloud',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
+})
+export class AppModule {}
